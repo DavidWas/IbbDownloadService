706e000b53efa8d904f6f77a34413570b366799f
DavidWas <david_was@mac.com>
2024-09-20 20:13:12 +0200

    adds constraint migration

---
+++ IbbDownloadService.NugetModule/Migrations/20240920180431_AddConstraint.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IbbDownloadService.NugetModule.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Nugets_Name_Version",
+                schema: "Nugets",
+                table: "Nugets",
+                columns: new[] { "Name", "Version" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Nugets_Name_Version",
+                schema: "Nugets",
+                table: "Nugets");
+        }
+    }
+}
